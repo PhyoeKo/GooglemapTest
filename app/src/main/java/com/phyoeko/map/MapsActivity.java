18960de0938934e7ef09a1c8ee1ed0bddfdc064c
Phyoe Ko <developer.skylark@skylarkmyanmar.com>
2019-06-25 11:27:47 +0630

    Initial commit

---
+++ app/src/main/java/com/phyoeko/map/MapsActivity.java
@@ -0,0 +1,82 @@
+package com.phyoeko.map;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+//        GoogleMapOptions options = new GoogleMapOptions();
+//
+//        options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
+//                .compassEnabled(false)
+//                .rotateGesturesEnabled(false)
+//                .tiltGesturesEnabled(false);
+//
+
+        // Add a marker in Sydney and move the camera
+
+        /**Zoom View
+         1: World
+         5: Landmass/continent
+         10: City
+         15: Streets
+         20: Buildings**/
+            LatLng myLatLng = new LatLng(16.7981421, 96.1494511);
+        CameraPosition myPosition = new CameraPosition.Builder()
+                .target(myLatLng).zoom(15).bearing(90).tilt(30).build();
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//
+        mMap.setMyLocationEnabled(true);
+        mMap.addMarker(new MarkerOptions().position(myLatLng).title("Sample Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.animateCamera(
+                CameraUpdateFactory.newCameraPosition(myPosition));
+
+
+    }
+
+
+}
